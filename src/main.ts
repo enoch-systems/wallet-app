7f1351eaf08ba99d69b6b6cff2fbe3d00d585dec
Enoch Chukwudi <enochc.official@gmail.com>
2026-06-13 16:57:04 +0100

    feat: add CORS, remove old HTML files (using Next.js frontend)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors({
+    origin: [
+      'http://localhost:3001',
+      'http://localhost:3000',
+      'https://wallet-app-xqtq.onrender.com',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   
   await app.listen(process.env.PORT ?? 3000);
